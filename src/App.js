3ba718060e9f4878097e9d06054142ecbe87a3ff
pavan1143 <103113372+pavan1143@users.noreply.github.com>
2023-01-10 18:09:33 +0530

    craete pages and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import {BrowserRouter as Router,Routes,Route}from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
 
 function App() {
   return (
     <>
-        <h1>Hello world
-
-        </h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/sign-In" element={<SignIn/>}/>
+          <Route path="/sign-Up" element={<SignUp/>}/>
+          <Route path="/forgotPassword" element={<ForgotPassword/>}/>
+          <Route path="/offers" element={<Offers/>}/>
+        </Routes>
+      </Router>
     </>
-  )
+  ) 
 }
 
 export default App

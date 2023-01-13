bfaea4e57357703bf937863316064aec5f8fc6a9
pavan1143 <103113372+pavan1143@users.noreply.github.com>
2023-01-13 15:10:25 +0530

    login redials

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import CreateListing from "./pages/CreateListing";
 function App() {
   return (
     <>
@@ -15,11 +17,14 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home/>}/>
+          <Route path="/profile" element={<PrivateRoute/>}>
           <Route path="/profile" element={<Profile/>}/>
+          </Route>
           <Route path="/sign-In" element={<SignIn/>}/>
           <Route path="/sign-Up" element={<SignUp/>}/>
           <Route path="/forgotPassword" element={<ForgotPassword/>}/>
           <Route path="/offers" element={<Offers/>}/>
+          <Route path="/Create-listing" element={<CreateListing/>}/>
         </Routes>
       </Router>
       <ToastContainer
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,66 @@
-import React from 'react'
-import { useLocation,useNavigate } from 'react-router'
-export { useLocation } from 'react-router-dom'
-
-
-
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default function Header() {
-    const location = useLocation();
-    const navigate = useNavigate()
-    console.log (location.pathname);
-    function pathMathRote(route){
-      if(route === location.pathname){
-        return true
+  const [pageState, setPageState] = useState("Sign in");
+  const location = useLocation();
+  const navigate = useNavigate();
+  const auth = getAuth();
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setPageState("Profile");
+      } else {
+        setPageState("Sign in");
       }
-      
+    });
+  }, [auth]);
+  function pathMatchRoute(route) {
+    if (route === location.pathname) {
+      return true;
     }
+  }
   return (
-    <div className='bd=white border-b shadow-sm sticky top-0 z-50'>
-        <header className='flex justify-between items-center px-3 max-w-6pxl max-auto' >
-            <div>
-                <img src="https://graphicsfamily.com/wp-content/uploads/edd/2021/08/Free-Farm-Logo-Vector-Agriculture-Logo-Template-Download-1536x864.jpg" alt="logo"  className='h-20 cursor-pointer' onClick={()=>navigate("/") }/>
-            </div>
-            <div>
-              <ul className='flex space-x-20'>
-                <li className={'cursor-pointer py-3 text-sm font-semibold text-gray-400   border-b-[3px] border-b-transparent ${pathMathRoute("/) && text-black  border-b-red-500 }'} 
-                onClick={()=>navigate("/")}
-                >Home</li>
-                <li className={'cursor-pointer py-3 text-sm font-semibold text-gray-400   border-b-[3px] border-b-transparent ${pathMathRoute("/Offers) && text-black border-b-red-500}'}
-                 onClick={()=>navigate("/Offers")}
-                 >Offers</li>
-                <li className={'cursor-pointer py-3 text-sm font-semibold text-gray-400   border-b-[3px] border-b-transparent ${pathMathRoute("/Sign in) && text-black  border-b-red-500 }'}
-                onClick={()=>navigate("/Sign-in")}
-                >Sign in</li>  
-              </ul>
-            </div>
-        </header>
+    <div className="bg-white border-b shadow-sm sticky top-0 z-40">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src="https://pbs.twimg.com/profile_banners/4114250602/1555197758/1080x360"
+            alt="logo"
+            className="h-5 cursor-pointer"
+            onClick={() => navigate("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                (pathMatchRoute("/sign-in") || pathMatchRoute("/profile")) &&
+                "text-black border-b-red-500"
+              }`}
+              onClick={() => navigate("/profile")}
+            >
+              {pageState}
+            </li>
+          </ul>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
